f77133c70f2f2d7d5e579f20e53d1127c7136156
Sharon <156696215+Biwott362@users.noreply.github.com>
2025-05-16 10:46:35 +0300

    Added firebase config from firebase studio

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  firebaseConfig: {}
+  firebaseConfig: {
+    apiKey: "AIzaSyBNgpld7Z0ajfedfJgbTTizM1r2bb99cvw",
+    authDomain: "angular-vertex-lab8.firebaseapp.com",
+    projectId: "angular-vertex-lab8",
+    storageBucket: "angular-vertex-lab8.firebasestorage.app",
+    messagingSenderId: "549652903825",
+    appId: "1:549652903825:web:5fcfb8fdd43d302b614c30"
+  }
 };
